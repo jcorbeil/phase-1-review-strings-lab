3f0778bec4588537780ccfebf4e153f9e3921a1e
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-06-05 22:49:19 -0400

    finished phase-1-review-atrings-lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Declare and define the currentUser variable
+const currentUser = "John Smith";
+
+// Construct welcomeMessage using string concatenation
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+// Construct excitedWelcomeMessage using string concatenation and uppercase
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+// Construct shortGreeting using string concatenation and extracting the first initial
+const shortGreeting = "Welcome, " + currentUser[0] + "!";
